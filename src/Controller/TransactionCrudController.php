8a1f0e11107c1ea8beba64ccdf6cfa2f1bd992be
Anett David <davidanett@gmail.com>
2022-09-12 20:28:25 +0200

    Create routes and add template files

--- src/Controller/TransactionCrudController.php
+++ src/Controller/TransactionCrudController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Transaction;
+use App\Repository\TransactionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TransactionCrudController extends AbstractController
 {
     /**
-     * @Route("/transaction/crud", name="app_transaction_crud")
+     * @Route("/nouveau", name="create_transaction")
+     * @Route("/modification/{id}", name="edit_transaction", requirements={"id":"\d+"})
      */
-    public function index(): Response
+    public function createEditTransaction(Transaction $id = null, Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('transaction_crud/index.html.twig', [
+        return $this->render('transaction_crud/createEditTransaction.html.twig', [
+            'page_title' => $id ? 'Mise à jour de la transaction' : 'Ajouter une transaction',
+        ]);
+    }
+
+    /**
+     * @Route("/supprimer/{id}", name="delete_transaction", requirements={"id":"\d+"})
+     */
+    public function deleteTransaction(Transaction $id, Request $request, EntityManagerInterface $manager): Response
+    {
+        return $this->render('transaction_crud/deleteTransaction.html.twig', [
             'controller_name' => 'TransactionCrudController',
         ]);
     }
