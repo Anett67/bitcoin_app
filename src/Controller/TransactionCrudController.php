b4e6f4d80777f0dc0ee9e38d27937253142186ed
Anett David <davidanett@gmail.com>
2022-09-19 21:35:49 +0200

    Use the same controller and form for editing and creating

--- src/Controller/TransactionCrudController.php
+++ src/Controller/TransactionCrudController.php
@@ -18,7 +18,7 @@ class TransactionCrudController extends AbstractController
     /**
      * @Route("/nouveau", name="create_transaction")
      */
-    public function createTransaction(Request $request, EntityManagerInterface $manager): Response
+    public function createEditTransaction(Request $request, EntityManagerInterface $manager, TransactionRepository $transactionRepository): Response
     {
         if(!$this->isGranted('IS_AUTHENTICATED_FULLY')){
             return $this->redirectToRoute('app_login');
@@ -32,6 +32,19 @@ class TransactionCrudController extends AbstractController
             $quantity = $transactionForm->get('quantity')->getData();
             $unit_price = $transactionForm->get('price')->getData();
 
+            if ($transactionToEdit = $transactionRepository->findOneBy([
+                'user' => $this->getUser(), 
+                'crypto' => $transactionForm->get('crypto')->getData()
+            ])) {
+                $transactionToEdit->setQuantity($transactionToEdit->getQuantity() + $quantity);
+                $transactionToEdit->setPrice($transactionToEdit->getPrice() + ($quantity * $unit_price));
+                $transactionToEdit->setUpdatedAt(new DateTimeImmutable());
+                $manager->persist($transactionToEdit);
+                $manager->flush();
+
+                return $this->redirectToRoute('dashboard');
+            }
+
             $transaction->setCreatedAt(new DateTimeImmutable());
             $transaction->setUser($this->getUser());
             $transaction->setUpdatedAt(new DateTimeImmutable());
@@ -44,46 +57,7 @@ class TransactionCrudController extends AbstractController
         }
 
         return $this->render('transaction_crud/createEditTransaction.html.twig', [
-            'page_title' => 'Ajouter une transaction',
-            'action' => 'create', 
-            'form' => $transactionForm->createView()
-        ]);
-    }
-    /**
-     * @Route("/modification", name="edit_transaction")
-     */
-    public function editTransaction(Request $request, EntityManagerInterface $manager, TransactionRepository $transactionRepository): Response
-    {
-        if(!$this->isGranted('IS_AUTHENTICATED_FULLY')){
-            return $this->redirectToRoute('app_login');
-        }
-
-        $transaction = new Transaction();
-        $transactionForm = $this->createForm(TransactionEditionType::class, $transaction);
-        $transactionForm->handleRequest($request);
-
-        if($transactionForm->isSubmitted() && $transactionForm->isValid()){
-
-            $transactionToEdit = $transactionRepository->findOneBy([
-                'user' => $this->getUser(), 
-                'crypto' => $transactionForm->get('crypto')->getData()
-            ]); 
-
-            $quantity = $transactionForm->get('quantity')->getData();
-            $unit_price = $transactionForm->get('price')->getData(); 
-
-            $transactionToEdit->setQuantity($transactionToEdit->getQuantity() + $quantity);
-            $transactionToEdit->setPrice($transactionToEdit->getPrice() + ($quantity * $unit_price));
-            $transactionToEdit->setUpdatedAt(new DateTimeImmutable());
-            $manager->persist($transactionToEdit);
-            $manager->flush();
-
-            return $this->redirectToRoute('dashboard');
-        }
-
-        return $this->render('transaction_crud/createEditTransaction.html.twig', [
             'page_title' => 'Ajouter un montant',
-            'action' => 'edit',
             'form' => $transactionForm->createView()
         ]);
     }
