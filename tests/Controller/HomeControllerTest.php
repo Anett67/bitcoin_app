3c7a4ad63cd74d30bda7bb1d382fd4002c35ea95
Anett David <davidanett@gmail.com>
2022-10-01 22:51:12 +0200

    Add earnings page test

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
@@ -18,5 +19,23 @@ class HomeControllerTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', '/gains');
         $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEarningsPageIsUp() 
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('johndoe@mail.fr');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $crawler = $client->request('GET', '/gains');
+        $this->assertSame(1, $crawler->filter('html:contains("Vos gains")')->count());
+        $this->assertSelectorExists('canvas');
     }
 }
